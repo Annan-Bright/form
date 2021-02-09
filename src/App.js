7928c0893c0ceb4d64349151d05d060cb40d2025
Bright Annan <brightannan5@gmail.com>
2021-02-09 22:36:24 +0000

    change function component to state, event and form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class MyForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { username: "" };
+  }
+  mySubmitHandler = (event) => {
+    event.preventDefault();
+    alert("You are submitting " + this.state.username);
+  };
+  myChangeHandler = (event) => {
+    this.setState({ username: event.target.value });
+  };
+  render() {
+    return (
+      <form onSubmit={this.mySubmitHandler}>
+        <h1>Hello {this.state.username}</h1>
+        <p>Enter your name, and submit:</p>
+        <input type="text" onChange={this.myChangeHandler} />
+        <input type="submit" />
+      </form>
+    );
+  }
 }
 
-export default App;
+export default MyForm;
